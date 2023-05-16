460a2bf625f06cb03dc88cddd7372ac2c87a75da
Mazo667 <68477400+Mazo667@users.noreply.github.com>
2023-05-16 15:37:27 -0300

    Arreglue el directorio donde leia

--- JAVA_BASICO/src/Properties/PropertiesUtilMain.java
+++ JAVA_BASICO/src/Properties/PropertiesUtilMain.java
@@ -4,8 +4,9 @@ import java.util.Properties;
 public class PropertiesUtilMain {
 
 	public static void main(String[] args) {
-		Properties props = PropertiesUtil.getproperties("persona.properties");
+		Properties props = PropertiesUtil.getproperties("C:\\Users\\MAXIMILIANO\\git\\JAVA_BASICO\\JAVA_BASICO\\src\\Properties\\persona.properties");
 		
+		System.out.println(props.getProperty("nombre"));
 		//Si el archivo de propiedades fuera exclisivo para la clase Aplicacion deberia invocarse asi
 		//Properties props = PropertiesUtil.getProperties(Aplicacion.class);
 
