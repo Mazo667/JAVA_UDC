a5b3b4a16693a8a7dca14ee9f2fe4a0a9427b49b
Mazo667 <68477400+Mazo667@users.noreply.github.com>
2023-05-16 00:59:31 -0300

    Importante es un metodo para fabricar objetos comunicadores (Factory Method)

--- JAVA_BASICO/src/Interfaces/EjemploConFactoriasDeObjetos/ComunicadorManager.java
+++ JAVA_BASICO/src/Interfaces/EjemploConFactoriasDeObjetos/ComunicadorManager.java
@@ -1,7 +1,7 @@
 package Interfaces.EjemploConFactoriasDeObjetos;
 
 public class ComunicadorManager {
-	
+	//Factory Method
 	//Obtendremos una instancia de Comunicador para enviar un mensaje a traves de esta
 	public static Comunicador crearComunicador() {
 		//una paloma mensajera es un comunicador
