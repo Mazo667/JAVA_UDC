ec189628111777f2e13d9118d7ad1c44ecd3cf3f
Mazo667 <68477400+Mazo667@users.noreply.github.com>
2023-05-16 23:09:25 -0300

    Agregue ejemplo de Excepcion Declarativa

--- JAVA_BASICO/src/Excepciones/TestLogin.java
+++ JAVA_BASICO/src/Excepciones/TestLogin.java
@@ -5,14 +5,15 @@ public class TestLogin {
 	public static void main(String[] args) {
 		
 		try {
+		//AplicacionDeclarativa app2 = new AplicacionDeclarativa();
 			Aplicacion app = new Aplicacion();
 			//intentamos el login
 			Usuario u = app.Login("maxi", "admin");
-			System.out.println("Haz logueado correctamente");
+			//Usuario u2 = app2.Login("maxi", "clave");
 			System.out.println("Usuario: "+u.getUsername()+"\nEmail: "+u.getEmail()+"\nNombre: "+u.getNombre());
 		}catch (Exception ex){
 			//Ocurrio un error
-			System.out.println("Fuera de servicio");
+			System.out.println("Ocurrio un error: ");
 			System.out.println("Intente mas tarde");
 			System.out.println(ex.getMessage());
 		}
