b315785c69905a3d282046a702ecb989ed745fa3
Mazo667 <maxifava667@gmail.com>
2023-06-03 22:39:12 -0300

    Metodo para imprimir las ventas con una fecha y un vendedor

--- JAVA_BASICO/src/BaseDeDatosJDBC/Practica/VentaConsultaPago.java
+++ JAVA_BASICO/src/BaseDeDatosJDBC/Practica/VentaConsultaPago.java
@@ -1,9 +1,13 @@
 package BaseDeDatosJDBC.Practica;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class VentaConsultaPago {
 	
@@ -27,19 +31,22 @@ public class VentaConsultaPago {
 			Class.forName(drv);
 			//establezco la conexion
 			con = DriverManager.getConnection(url, usr, pwd);
-		//Los valores paramatrizados se representan con signos de interrogacion
-			String sql="SELECT * FROM localidad WHERE nombre LIKE ?";
-			pstm = con.prepareStatement(sql);
-		//Asignamos un valor para el parametro numero 1
-			pstm.setString(1, "TRELEW%");
-			rs = pstm.executeQuery();
 			
-			System.out.println("codigo | nombre");
-			while(rs.next() ) {
-				int codigo = rs.getInt("codigo");
-				String nombre = rs.getString("nombre");
-				System.out.println(codigo+",  "+nombre);
-			}
+			
+			//Seteo la fecha que quiero obtener
+			GregorianCalendar gc = new GregorianCalendar();
+			gc.set(Calendar.YEAR,2015);
+			gc.set(Calendar.MONTH,8-1);
+			gc.set(Calendar.DAY_OF_MONTH,1);
+			
+			long ts = gc.getTimeInMillis();
+			Date fecha = new Date(ts);
+			//Codigo Vendedor
+			String vendedor = "JUA";//Efectivo
+			String vendedor2 = "GOM";//Tarjeta
+			
+			//Llamamos al metodo para hacer la consulta
+			ConsultaPago(con,vendedor2,fecha);
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -53,6 +60,55 @@ public class VentaConsultaPago {
 				e.printStackTrace();
 				throw new RuntimeException(e);
 			}
+		}
+	}
+
+	private static void ConsultaPago(Connection con,String vendedor,Date d) {
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		try {
+			String sql ="SELECT * FROM venta "
+					+"WHERE FECHA = ? AND vendedor = ?;";
+			pstm = con.prepareStatement(sql);
+			pstm.setDate(1, d);
+			pstm.setString(2, vendedor);
+			//Ejecuto el Update
+			rs = pstm.executeQuery();
+			
+			System.out.println("ID  |  FECHA   | CLIENTE  | CANTIDAD | PRODUCTO | TOTAL  | VENDEDOR | TARJETA");
+			while(rs.next() ) {
+				int id = rs.getInt("id");
+				Date fecha = rs.getDate("fecha");
+				int cliente = rs.getInt("cliente");
+				int cant = rs.getInt("cantidad");
+				String producto = rs.getString("producto");
+				Float total = rs.getFloat("total");
+				String ven = rs.getString("vendedor");
+				int tar = rs.getInt("tarjeta");
+				
+				if(tar != 0) {
+					System.out.println(id+"|"+fecha+"| "+cliente+" |    "+cant+"    |   "+
+							producto+"  | "+total+" | "+ven+"      |  "+tar);
+				}else {
+					System.out.println(id+"|"+fecha+"| "+cliente+" |    "+cant+"    |   "+
+							producto+"  | "+total+"  | "+ven+"     |  Efectivo");
+				}
+				
+				
+			}
+			
+		}catch(Exception e) {
+			   e.printStackTrace();
+		        
+		}finally {
+			 // Cierro el PreparedStatement
+	        if (pstm != null) {
+	            try {
+	                pstm.close();
+	            } catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	        }
 		}
 	}
 }
