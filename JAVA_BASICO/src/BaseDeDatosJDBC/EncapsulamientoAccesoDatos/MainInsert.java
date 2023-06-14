20faf181bbf3179437983ac5ea24a797528084f8
Mazo667 <68477400+Mazo667@users.noreply.github.com>
2023-06-14 00:14:09 -0300

    Agrego el metodo insert a ClienteDAO para poder insertar un objeto Cliente

---
+++ JAVA_BASICO/src/BaseDeDatosJDBC/EncapsulamientoAccesoDatos/MainInsert.java
@@ -0,0 +1,35 @@
+package BaseDeDatosJDBC.EncapsulamientoAccesoDatos;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import BaseDeDatosJDBC.AdminstrarConexion.JdbcUtil;
+
+public class MainInsert {
+	public static void main(String[] args) throws SQLException {
+		ClienteDAO dao = new ClienteDAO();
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.set(Calendar.YEAR,1955);
+		gc.set(Calendar.MONTH,02);
+		gc.set(Calendar.DAY_OF_MONTH,14);
+		
+		long ts = gc.getTimeInMillis();
+		Date d = new Date(ts);
+		
+		Cliente cli = new Cliente();
+		cli.setApellido("ARGENTO");
+		cli.setNombre("PEPE");
+		cli.setDni(22114222);
+		cli.setDomicilio("25 de Mayo 328");
+		cli.setFechaNac(d);
+		cli.setLocalidad(2);
+		cli.setSexo("M");
+		
+		dao.insertar(cli);
+		//Connection con = JdbcUtil3.getConnection();
+		//con.commit();
+	}
+}
