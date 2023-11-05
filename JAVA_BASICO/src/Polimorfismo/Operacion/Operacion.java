d72715e28fdbed76a83d981bc2f648e69b084443
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-11-05 20:40:35 -0300

    Create Operacion.java

---
+++ JAVA_BASICO/src/Polimorfismo/Operacion/Operacion.java
@@ -0,0 +1,16 @@
+public class Operacion {
+	protected int valor1;
+	protected int valor2;
+	protected int resultado;
+	
+	public Operacion(int valor1, int valor2) {
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+	}
+	
+	
+	public int operar() {
+		return resultado;
+	}
+
+}
